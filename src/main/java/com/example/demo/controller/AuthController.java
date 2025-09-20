5ea642e59784546c6b177d3dc4c8f6522873d4e8
nguyentanjr <tannguyendinh7@gmail.com>
2025-09-20 15:40:51 +0700

    Implement JWT Authentication

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,103 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.auth.JwtAuthenticationResponse;
+import com.example.demo.dto.auth.LoginRequest;
+import com.example.demo.dto.auth.RegisterRequest;
+import com.example.demo.dto.common.ApiResponse;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.RefreshToken;
+import com.example.demo.model.User;
+import com.example.demo.security.CookieUtils;
+import com.example.demo.service.AuthService;
+import com.example.demo.service.RefreshTokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private CookieUtils cookieUtils;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<JwtAuthenticationResponse>> register(
+            @Valid @RequestBody RegisterRequest registerRequest, HttpServletResponse response) {
+
+        log.info("Registering new user: {}", registerRequest.getUsername());
+
+
+        JwtAuthenticationResponse authResponse = authService.register(registerRequest);
+        cookieUtils.createRefreshTokenCookie(response, authResponse.getRefreshToken());
+        ApiResponse<JwtAuthenticationResponse> apiResponse = ApiResponse.created(
+                authResponse,"User registered and logged in successfully");
+        return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<JwtAuthenticationResponse>> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+
+        log.info("User attempt for user: {}", loginRequest.getUsername());
+
+        JwtAuthenticationResponse authResponse = authService.login(loginRequest);
+        cookieUtils.createRefreshTokenCookie(response, authResponse.getRefreshToken());
+        ApiResponse<JwtAuthenticationResponse> apiResponse = ApiResponse.success(authResponse, "Login successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ApiResponse<String>> logout(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Logout request");
+
+        String refreshToken = cookieUtils.getRefreshTokenFromCookie(request)
+                .orElseThrow(() -> new ResourceNotFoundException("Refresh Token not found in cookies"));
+        authService.logout(refreshToken);
+
+        cookieUtils.deleteRefreshTokenCookie(response);
+
+        ApiResponse<String> apiResponse = ApiResponse.success("Logout Successfully", "User logout successfully");
+
+        return ResponseEntity.ok(apiResponse);
+
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<ApiResponse<JwtAuthenticationResponse>> refreshToken(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Refresh token request");
+
+        String refreshToken = cookieUtils.getRefreshTokenFromCookie(request)
+                .orElse(null);
+
+        if(refreshToken == null) {
+            log.warn("No refresh token found in cookies");
+            throw new RuntimeException("Refresh token not found in cookies. Please login again.");
+        }
+        JwtAuthenticationResponse jwtAuthenticationResponse = authService.refreshToken(refreshToken);
+
+        cookieUtils.createRefreshTokenCookie(response, jwtAuthenticationResponse.getRefreshToken());
+
+        ApiResponse<JwtAuthenticationResponse> apiResponse = ApiResponse.success(jwtAuthenticationResponse, "Token refreshed successfully");
+
+        return ResponseEntity.ok(apiResponse);
+    }
+
+}
---
+++ src/main/java/com/example/demo/service/Impl/AuthServiceImpl.java
@@ -0,0 +1,131 @@
+package com.example.demo.service.Impl;
+
+
+import com.example.demo.dto.auth.JwtAuthenticationResponse;
+import com.example.demo.dto.auth.LoginRequest;
+import com.example.demo.dto.auth.RegisterRequest;
+import com.example.demo.dto.user.UserResponse;
+import com.example.demo.exception.EmailAlreadyExistsException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.exception.UsernameAlreadyExistsException;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.RefreshToken;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.security.JWTUtils;
+import com.example.demo.service.AuthService;
+import com.example.demo.service.RefreshTokenService;
+import io.micrometer.common.util.StringUtils;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Slf4j
+@Service
+@Transactional
+public class AuthServiceImpl implements AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public JwtAuthenticationResponse register(@Valid @RequestBody RegisterRequest registerRequest) {
+
+        log.info("Registering new user: {}", registerRequest.getUsername());
+        if(userRepository.existsByUsername(registerRequest.getUsername())) {
+            log.warn("Registration failed: username '{}' already exists", registerRequest.getUsername());
+            throw new UsernameAlreadyExistsException("Username has already exists");
+        }
+        if(userRepository.existsByEmail(registerRequest.getEmail())) {
+            log.warn("Registration failed: email '{}' already exists", registerRequest.getEmail());
+            throw new EmailAlreadyExistsException("Email has already exists");
+        }
+
+        User user = userMapper.toUser(registerRequest);
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        User savedUser = userRepository.save(user);
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        savedUser, null, savedUser.getAuthorities()
+                );
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        log.info("User '{}' registered successfully",registerRequest.getUsername());
+
+        String accessToken = jwtUtils.generateJwtToken(usernamePasswordAuthenticationToken);
+        String refreshToken = refreshTokenService.createRefreshToken(savedUser).getToken();
+        UserResponse userResponse = userMapper.toUserResponse(savedUser);
+
+        return new JwtAuthenticationResponse(accessToken, refreshToken, userResponse);
+    }
+
+    @Override
+    public JwtAuthenticationResponse login(@Valid @RequestBody LoginRequest loginRequest) {
+        log.info("User login: {}", loginRequest.getUsername());
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken
+                        (loginRequest.getUsername(), loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        
+        User user = (User) authentication.getPrincipal();
+        String accessToken = jwtUtils.generateJwtToken(authentication);
+        String refreshToken = refreshTokenService.createRefreshToken(user).getToken();
+        UserResponse userResponse = userMapper.toUserResponse(user);
+
+        return new JwtAuthenticationResponse(accessToken, refreshToken, userResponse);
+    }
+
+    @Override
+    public void logout(String refreshTokenString) {
+
+        log.info("Logout request");
+
+        RefreshToken refreshToken = refreshTokenService.findByToken(refreshTokenString)
+                .orElseThrow(() -> new ResourceNotFoundException("Refresh Token not found"));
+
+        refreshTokenService.revokeToken(refreshToken);
+
+        log.info("User logout successfully: {}", refreshToken.getUser().getUsername());
+    }
+
+    @Override
+    public JwtAuthenticationResponse refreshToken(String refreshTokenString) {
+        log.info("Refresh Token request");
+
+        RefreshToken refreshToken = refreshTokenService.findByToken(refreshTokenString)
+                .orElseThrow(() -> new ResourceNotFoundException("Refresh Token not found!"));
+
+        refreshToken = refreshTokenService.isRefreshTokenValid(refreshToken);
+
+        String accessToken = jwtUtils.generateJwtTokenFromUsername(refreshToken.getUser().getUsername());
+        String responseRefreshToken = refreshTokenService.rotateToken(refreshToken).getToken();
+        UserResponse userResponse = userMapper.toUserResponse(refreshToken.getUser());
+
+        return new JwtAuthenticationResponse(accessToken, responseRefreshToken, userResponse);
+    }
+}
